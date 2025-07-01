2c2cd3707278ab547c5cd1e42e8f0c25cf5cb421
Qoz156 <qozfahussain@gmail.com>
2025-07-01 15:11:04 +0100

    Using requests-html for javascript issue

--- main.py
+++ main.py
@@ -1,41 +1,12 @@
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
+from requests_html import HTMLSession
 
-url = 'https://devpost.com/hackathons'
 
+url = 'https://react-amazon-bestsellers-books-dy.netlify.app/'
 
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
-                  "(KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
-    "Accept-Language": "en-US,en;q=0.9",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-    "Referer": "https://www.google.com/",
-    "Connection": "keep-alive",
-}
+session = HTMLSession()
+response = session.get(url)
+soup = BeautifulSoup(response.content, 'html.parser')
 
-response = requests.get(url, headers=headers)
-
-print(response.status_code)
-if response.status_code == 200:
-    soup = BeautifulSoup(response.text, "html.parser")
-
-    # Find all hackathon blocks
-    hackathons = soup.find_all("div", class_="hackathon-tile")
-
-    for hackathon in hackathons:
-        title = hackathon.find("h3").text.strip()
-        link = "https://devpost.com" + \
-            hackathon.find("a", class_="tile-anchor")["href"]
-        dates = hackathon.find("div", class_="submission-period").text.strip()
-        location_span = hackathon.find("i", class_="fas fa-globe")
-        location = location_span.find_next(
-            "span").text.strip() if location_span else "Unknown"
-
-        print(f"Title: {title}")
-        print(f"Dates: {dates}")
-        print(f"Location: {location}")
-        print(f"Link: {link}")
-        print("-" * 50)
-
-else:
-    print("Scraping did not work.")
+print(soup.find_all('article', class_='book'))
